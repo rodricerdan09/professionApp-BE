a5c9d032c8a101e3e0b74233c9fcad0f5007b447
jhgakiyama <lp_japo@hotmail.com>
2020-07-15 23:56:24 -0300

    se agrega el decorador @api_view
    Signed-off-by: jhgakiyama <lp_japo@hotmail.com>

--- solicitudes/views.py
+++ solicitudes/views.py
@@ -1,59 +1,49 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Entidad
 from .serializers import EntidadSerializer
 
 
-class JSONResponse(HttpResponse):
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def entidad_list(request):
     if request.method == 'GET':
         entidades = Entidad.objects.all()
         entidades_serializer = EntidadSerializer(entidades, many=True)
-        return JSONResponse(entidades_serializer.data)
+        return Response(entidades_serializer.data)
     elif request.method == 'POST':
-        entidad_data = JSONParser().parse(request)
-        entidad_serializer = EntidadSerializer(data=entidad_data)
+        entidad_serializer = EntidadSerializer(data=request.data)
+
         if entidad_serializer.is_valid():
             entidad_serializer.save()
             print("hice un POST")
-            return JSONResponse(entidad_serializer.data, status=status.HTTP_201_CREATED)
+            return Response(entidad_serializer.data, status=status.HTTP_201_CREATED)
 
-        return JSONResponse(entidad_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(entidad_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def entidad_detail(request, pk):
     try:
         entidad = Entidad.objects.get(pk=pk)
     except Entidad.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         entidad_serializer = EntidadSerializer(entidad)
         print("hice un GET")
-        return JSONResponse(entidad_serializer.data)
+        return Response(entidad_serializer.data)
 
     elif request.method == 'PUT':
         print("hice un PUT")
-        entidad_data = JSONParser().parse(request)
-        entidad_serializer = EntidadSerializer(entidad, data=entidad_data)
+        entidad_serializer = EntidadSerializer(entidad, data=request.data)
         if entidad_serializer.is_valid():
             entidad_serializer.save()
-            return JSONResponse(entidad_serializer.data)
-        return JSONResponse(entidad_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(entidad_serializer.data)
+        return Response(entidad_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         print("hice un DELETE")
         entidad.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
